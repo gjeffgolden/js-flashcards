20ce53d18d48dc6dad2e6dd7f189eda3b951b9b6
gjeffgolden <gjgolden08@gmail.com>
2021-05-17 12:27:04 -0600

    Creating term definitions.

---
+++ src/assets/KeyTerms.tsx
@@ -0,0 +1,30 @@
+type KeyTerm = {
+    cardFront: string,
+    cardBack: string,
+    cardBack2: string
+}
+
+const keyTerms: KeyTerm[] = [
+    {
+        cardFront: "Static Generation (SSG)",
+        cardBack: "Pre-rendering method that generates HTML at build time.",
+        cardBack2: "Use if page can be rendered without relying on user input."
+    },
+    {
+        cardFront: "",
+        cardBack: "",
+        cardBack2: ""
+    },
+    {
+        cardFront: "",
+        cardBack: "",
+        cardBack2: ""
+    },
+    {
+        cardFront: "",
+        cardBack: "",
+        cardBack2: ""
+    },
+]
+
+export default keyTerms;
